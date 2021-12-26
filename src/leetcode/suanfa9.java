8473cb6d87991e932ed3ea972d9ae96d5f30dfb5
lukezc90 <bussi@sina.com>
2021-12-26 22:43:16 +0800

    2021-12-26 suanfa9 修改代码递归*

--- src/leetcode/suanfa9.java
+++ src/leetcode/suanfa9.java
@@ -20,19 +20,21 @@ public class suanfa9 {
 //        prev.next == l1 == null?l2:l1;
 //        return prehead.next;
     //}
-    //递归
-//    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-//        if (l1 == null) {
-//            return l2;
-//        } else if (l2 == null) {
-//            return l1;
-//        } else if (l1.val < l2.val) {
-//            l1.next = mergeTwoLists(l1.next, l2);
-//            return l1;
-//        } else {
-//            l2.next = mergeTwoLists(l1, l2.next);
-//            return l2;
-//        }
+    //*递归
+//    public ListNode Merge(ListNode list1,ListNode list2) {
+//       if(list1 == null){
+//           return list2;
+//       }
+//       if(list2 == null){
+//           return list1;
+//       }
+//       if(list1.val <= list2.val){
+//           list1.next = Merge(list1.next, list2);
+//           return list1;
+//       }else{
+//           list2.next = Merge(list1, list2.next);
+//           return list2;
+//       }
 //    }
 
 }
