f2dbc11082d8b76f1a6b2b23c771c455620fe7db
lukezc90 <bussi@sina.com>
2021-12-26 21:58:17 +0800

    2021-12-26 suanfa7 修改代码

--- src/leetcode/suanfa7.java
+++ src/leetcode/suanfa7.java
@@ -12,16 +12,20 @@ https://leetcode-cn.com/problems/linked-list-cycle/solution/huan-xing-lian-biao-
 
  */
 public class suanfa7 {
-    public boolean hasCycle(ListNode head) {
-        Set<ListNode> seen = new HashSet<ListNode>() ;
-        while(head != null){
-            if(!seen.add(head)){
-                return true;
-                }
-            //head = head.next();
-            }
-        return false;
-        }
-
-    }
+//    public boolean hasCycle(ListNode head) {
+//        if (head == null || head.next == null) {
+//            return false;
+//        }
+//        ListNode slow = head;
+//        ListNode fast = head.next;
+//        while (slow != fast) {
+//            if (fast == null || fast.next == null) {
+//                return false;
+//            }
+//            slow = slow.next;
+//            fast = fast.next.next;
+//        }
+//        return true;
+//    }
+}
 
