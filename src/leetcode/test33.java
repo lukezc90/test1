f1f4bfbec16c03f08b400b9f1c07e9b8169ff08c
lukezc90 <bussi@sina.com>
2022-01-10 23:08:08 +0800

    2022-1-10 合并2个有序链表

---
+++ src/leetcode/test33.java
@@ -0,0 +1,21 @@
+package leetcode;
+/*
+合并2个排序的链表
+ */
+public class test33 {
+//    public ListNode Merge(ListNode list1,ListNode list2) {
+//        if(list1 == null){
+//            return list2;
+//        }
+//        if(list2 == null){
+//            return list1;
+//        }
+//        if(list1.val <= list2.val){
+//            list1.next = Merge(list1.next,list2);
+//            return list1;
+//        }else{
+//            list2.next = Merge(list2.next,list1);
+//            return list2;
+//        }
+//    }
+}
